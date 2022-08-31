5018239de8f1cd71c97141d77ab2f5098781f864
Belyaev224 <belyaeveu.24@gmail.com>
2022-08-31 12:54:38 +0400

    готовое дз

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+double n = double.Parse(Console.ReadLine());
+
+Console.Write(n + " -> ");
+
+n = Math.Abs(n);
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write(Math.Pow(i, 3) + ", ");
+} 
